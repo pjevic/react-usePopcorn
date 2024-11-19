edbe9abd1130f39fc04130f6d00cecdfd4694cda
Milan Pjevic <milanpjevic@icloud.com>
2024-11-19 13:33:45 +0100

    Loader

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Search from "./components/Search";
 import NumResults from "./components/NumResults";
 import Main from "./components/Main";
 import Box from "./components/Box";
+import Loader from "./components/Loader";
 import MovieList from "./components/MovieList";
 import WatchedSummary from "./components/WatchedSummary";
 import WatchedMoviesList from "./components/WatchedMoviesList";
@@ -18,12 +19,15 @@ const KEY = "77664bd9";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`);
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -35,9 +39,7 @@ export default function App() {
         <NumResults movies={movies} />
       </Navbar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
