9d9c7fe692f998ffb78d57e96cf50375eefcaab0
Milan Pjevic <milanpjevic@icloud.com>
2024-11-19 12:31:24 +0100

    Fetching Data: useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
@@ -13,9 +13,17 @@ import WatchedMoviesList from "./components/WatchedMoviesList";
 
 import { tempMovieData, tempWatchedData } from "./data/Data";
 
+const KEY = "77664bd9";
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
 
   return (
     <>
