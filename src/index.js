12b5c810752cdf0b9be192bcd614f17979e04521
Milan Pjevic <milanpjevic@icloud.com>
2024-11-19 11:11:10 +0100

    Back to App

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 import App from "./App";
-
-import StarRating from "./components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okey", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="violet" className="test" defaultRating={3} />
+    <App />
   </React.StrictMode>
 );
