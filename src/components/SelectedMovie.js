7a3edafa2fc5fef3325a93ef6398c2ac2c2c689d
Milan Pjevic <milanpjevic@icloud.com>
2024-11-20 09:05:42 +0100

    Chaninging Page Title

--- src/components/SelectedMovie.js
+++ src/components/SelectedMovie.js
@@ -67,6 +67,14 @@ export default function SelectedMovie({
     [selectedID]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
