3e849e69312af8d5025f5afc3afe88f608ee7f84
Milan Pjevic <milanpjevic@icloud.com>
2024-11-20 11:05:51 +0100

    Cleaning Up Data Fetching

--- src/App.js
+++ src/App.js
@@ -43,22 +43,28 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
-          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+          const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {
+            signal: controller.signal,
+          });
           if (!res.ok) throw new Error("Something went wrong with fetching movies.");
 
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movi not found.");
 
-          console.log(data.Search);
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -69,6 +75,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
